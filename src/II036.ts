18ee2ee1a14563b1065e352dbaff4c6a9c9a4b1f
刃心 <king-ty@foxmail.com>
2023-02-09 18:06:12 +0800

    Finish II036

---
+++ src/II036.ts
@@ -0,0 +1,26 @@
+function evalRPN(tokens: string[]): number {
+    let numSt: number[] = [];
+    for (let token of tokens) {
+        let temp = Number(token);
+        if (!isNaN(temp)) {
+            numSt.push(temp);
+        } else {
+            let y = numSt.pop() as number, x = numSt.pop() as number;
+            switch (token) {
+                case "+":
+                    numSt.push(x + y);
+                    break;
+                case "-":
+                    numSt.push(x - y);
+                    break;
+                case "*":
+                    numSt.push(x * y);
+                    break;
+                case "/":
+                    numSt.push((x - x % y) / y);
+                    break;
+            }
+        }
+    }
+    return numSt.pop() as number;
+};
