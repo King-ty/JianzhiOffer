0f9790c6b2621a628bdbc38c5275703163b49f88
刃心 <king-ty@foxmail.com>
2023-02-24 16:24:24 +0800

    Finish II068

---
+++ src/II068.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int searchInsert(vector<int>& nums, int target) {
+    return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+  }
+};
