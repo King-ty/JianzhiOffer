d2144fa5a34e18159b98e070a7b81ea79e257ff2
刃心 <king-ty@foxmail.com>
2023-03-01 14:42:48 +0800

    Finish II088

---
+++ src/II088.rs
@@ -0,0 +1,15 @@
+struct Solution;
+
+impl Solution {
+    pub fn min_cost_climbing_stairs(mut cost: Vec<i32>) -> i32 {
+        use std::cmp::min;
+        cost.push(0);
+        let (mut f0, mut f1, mut f2) = (0, cost[0], cost[1]);
+        for i in 2..cost.len() {
+            f0 = f1;
+            f1 = f2;
+            f2 = min(f0, f1) + cost[i];
+        }
+        f2
+    }
+}
