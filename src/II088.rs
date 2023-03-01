686ce75a47724596d1dcb54b28c7131302b727d7
刃心 <king-ty@foxmail.com>
2023-03-01 14:44:03 +0800

    Refact II088

--- src/II088.rs
+++ src/II088.rs
@@ -4,7 +4,8 @@ impl Solution {
     pub fn min_cost_climbing_stairs(mut cost: Vec<i32>) -> i32 {
         use std::cmp::min;
         cost.push(0);
-        let (mut f0, mut f1, mut f2) = (0, cost[0], cost[1]);
+        let mut f0;
+        let (mut f1, mut f2) = (cost[0], cost[1]);
         for i in 2..cost.len() {
             f0 = f1;
             f1 = f2;
